c2d1cdaa15f3beaa9d162107272163e56cf16548
Raphaël Thériault <raphael.theriault@solarwinds.com>
2022-11-30 15:17:37 -0800

    support TEST_TRUSTEDPATH env

--- scripts/env.js
+++ scripts/env.js
@@ -7,6 +7,7 @@ if (fs.existsSync(envPath)) {
   const contents = fs.readFileSync(envPath, "utf8")
   const kvs = contents
     .split("\n")
+    .filter((l) => !l.startsWith("#"))
     .map((l) => l.split("=", 2).map((s) => s.trim()))
     .filter((l) => l.length == 2)
   Object.assign(process.env, Object.fromEntries(kvs))
