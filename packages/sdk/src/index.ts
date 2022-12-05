effc7108ebefc0df10018272c4bbd1f81348a280
Raphaël Thériault <raphael.theriault@solarwinds.com>
2022-12-05 10:56:05 -0800

    basic sampler

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,7 +1,9 @@
 import { NodeSDK } from "@opentelemetry/sdk-node"
+import { type Sampler } from "@opentelemetry/sdk-trace-base"
 import * as os from "os"
 
 import { type SwoConfiguration, init } from "./config"
+import { SwoSampler } from "./sampler"
 
 export const SUPPORTED_PLATFORMS = ["linux-arm64", "linux-x64"] as const
 export const CURRENT_PLATFORM_SUPPORTED = (
@@ -10,9 +12,13 @@ export const CURRENT_PLATFORM_SUPPORTED = (
 
 export class SwoSDK extends NodeSDK {
   constructor(config: SwoConfiguration) {
+    let sampler: Sampler | undefined = undefined
+
     if (CURRENT_PLATFORM_SUPPORTED) {
       try {
         const _reporter = init(config)
+
+        sampler = new SwoSampler(config)
       } catch (error) {
         console.warn(
           "swo initialization failed, no traces will be collected. check your configuration to ensure it is correct.",
@@ -28,6 +34,7 @@ export class SwoSDK extends NodeSDK {
 
     super({
       ...config,
+      sampler,
     })
   }
 }
