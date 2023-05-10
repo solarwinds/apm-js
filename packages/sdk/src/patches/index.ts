2899c0365254773ed080926fa177d09a15fff9b4
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-05-09 17:05:48 -0700

    document expected config patch file structure

--- packages/sdk/src/patches/index.ts
+++ packages/sdk/src/patches/index.ts
@@ -12,6 +12,18 @@ export function patch(
   options: Options,
 ) {
   const patcher = new ConfigPatcher()
+
+  /**
+   * Every instrumentation config patch should live in its own file in this directory.
+   *
+   * It should export a single `patch(patcher: ConfigPatcher, options: Options): void` function
+   * which calls `patcher.patch` and will itself be called here.
+   *
+   * Patches will usually follow the same format of conditionally importing the constructor
+   * of the instrumentation they patch and return early if the package is not installed
+   * as every instrumentation is optional.
+   */
   http.patch(patcher, options)
+
   patcher.apply(instrumentations)
 }
