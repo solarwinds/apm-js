e1dce3a76ad385b829b6d738f2270830c6793004
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-05-26 10:18:39 -0700

    improve naming and comments

--- packages/bindings/src/metrics/event_loop.hh
+++ packages/bindings/src/metrics/event_loop.hh
@@ -18,29 +18,44 @@ The latency is communicated to the JS side using a callback. Since the latency i
 of the JS context where it's not possible to call JS functions, a thread-safe function is used which
 will push the call on a queue and schedule it to be executed with all the other callbacks on the
 next loop iteration.
+
+The overall process goes something like
+- JS calls `setCallback`
+  - The first argument is the JS callback function which receives the latency as its argument
+  - The second argument is the granularity
+- The `set_callback` C++ implementation of `setCallback` runs
+  - First it cleans up any previous state, and returns early if the first argument is null
+  - Otherwise it stores the callback passed to it as first argument in a thread-safe function
+  - It then initialises the libuv handles and `EventLoopData` and registers the libuv callbacks
+    `on_prepare` and `on_check` to run every loop iteration on their respective steps
+  - Every `granularity` iterations `on_check` will use the thread-safe function to schedule
+    the JS callback passed to `setCallback` earlier to be called with the latency
+
+For instance the following would log the latency every 10 iterations
+setCallback((latency) => console.log(latency), 10)
 */
 
 namespace {
 
-struct CallbackData {
+struct JsData {
     uint64_t latency;
 };
 
 // This is the native code that is put on a queue to be executed from a JS context, from here we are
 // able to create JS values and call the actual JS callback
-void call_js_callback(Napi::Env env, Napi::Function cb, std::nullopt_t*, CallbackData* data) {
+void call_js_callback(Napi::Env env, Napi::Function js_callback, std::nullopt_t*, JsData* data) {
     if (env != nullptr) {
-        cb.Call({Napi::Number::New(env, data->latency)});
+        js_callback.Call({Napi::Number::New(env, data->latency)});
     }
     delete data;
 }
 
-typedef Napi::TypedThreadSafeFunction<std::nullopt_t, CallbackData, call_js_callback> js_callback;
+typedef Napi::TypedThreadSafeFunction<std::nullopt_t, JsData, call_js_callback> ThreadSafeFunction;
 
 // This is stored as attached data on the libuv handles
 struct EventLoopData {
     // Thread-safe function
-    js_callback callback;
+    ThreadSafeFunction js_callback_scheduler;
     // Number of iterations to sum up before scheduling the callback
     unsigned int granularity;
 
@@ -62,7 +77,7 @@ static uv_check_t check_handle{};
 // are we currently measuring
 static bool set{false};
 
-void prepare_callback(uv_prepare_t* handle) {
+void on_prepare(uv_prepare_t* handle) {
     uint64_t prepare_time = uv_hrtime();
 
     auto data = static_cast<EventLoopData*>(handle->data);
@@ -72,7 +87,7 @@ void prepare_callback(uv_prepare_t* handle) {
     data->poll_timeout = uv_backend_timeout(uv_default_loop());
 }
 
-void check_callback(uv_check_t* handle) {
+void on_check(uv_check_t* handle) {
     uint64_t check_time = uv_hrtime();
 
     auto data = static_cast<EventLoopData*>(handle->data);
@@ -100,7 +115,7 @@ void check_callback(uv_check_t* handle) {
         return;
     }
 
-    data->callback.BlockingCall(new CallbackData{data->latency});
+    data->js_callback_scheduler.BlockingCall(new JsData{data->latency});
     data->latency = 0;
     data->count = 0;
 }
@@ -117,14 +132,14 @@ Napi::Value set_callback(swo::CallbackInfo const info) {
 
         // need to release the thread-safe function first to decrease its reference count so it
         // actually gets freed
-        data->callback.Release();
+        data->js_callback_scheduler.Release();
         delete data;
     }
 
     if (!arg.IsNull()) {
         // create the thread-safe function with its reference count set to 1
-        auto callback =
-            js_callback::New(info, arg.As<Napi::Function>(), "Event Loop Callback", 0, 1);
+        auto js_callback_scheduler =
+            ThreadSafeFunction::New(info, arg.As<Napi::Function>(), "Event Loop Callback", 0, 1);
         auto granularity = info.arg<unsigned int>(1);
 
         // re.initialise the libuv handles
@@ -133,7 +148,7 @@ Napi::Value set_callback(swo::CallbackInfo const info) {
 
         // allocate fresh data to not have skewed measurements if the granularity changed
         auto data = new EventLoopData{
-            .callback = std::move(callback),
+            .js_callback_scheduler = std::move(js_callback_scheduler),
             .granularity = granularity,
             .prev_check_time = 0,
             .count = 0,
@@ -141,9 +156,9 @@ Napi::Value set_callback(swo::CallbackInfo const info) {
         prepare_handle.data = data;
         check_handle.data = data;
 
-        // actually enable the callbacks
-        uv_prepare_start(&prepare_handle, prepare_callback);
-        uv_check_start(&check_handle, check_callback);
+        // actually enable the libuv callbacks
+        uv_prepare_start(&prepare_handle, on_prepare);
+        uv_check_start(&check_handle, on_check);
 
         set = true;
     } else {
@@ -155,6 +170,7 @@ Napi::Value set_callback(swo::CallbackInfo const info) {
 
 } // namespace
 
+// module initialisation, defines the `setCallback` function
 swo::Object event_loop(swo::Object exports) {
     exports.set("setCallback", set_callback);
     return exports;
