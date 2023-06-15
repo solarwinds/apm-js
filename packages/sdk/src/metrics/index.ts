aaf27eeca5d5327abb63f1518889d5b838016fb5
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-06-15 10:25:00 -0700

    cpu and memory metrics

--- packages/sdk/src/metrics/index.ts
+++ packages/sdk/src/metrics/index.ts
@@ -1,8 +1,10 @@
 import { metrics } from "@opentelemetry/api"
 import { lazy } from "@swotel/lazy"
 
+import * as cpu from "./cpu"
 import * as eventLoop from "./event-loop"
 import * as gc from "./gc"
+import * as memory from "./memory"
 
 /* eslint-disable-next-line ts/no-var-requires */
 const pkg = require("../../package.json") as { name: string; version: string }
@@ -10,10 +12,14 @@ const pkg = require("../../package.json") as { name: string; version: string }
 export const meter = lazy(() => metrics.getMeter(pkg.name, pkg.version))
 
 export function start() {
+  cpu.start()
   eventLoop.start()
   gc.start()
+  memory.start()
 }
 export function stop() {
+  cpu.stop()
   eventLoop.stop()
   gc.stop()
+  memory.stop()
 }
