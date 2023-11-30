65f6d8521e23658f8221dd314f2dfa65fa1527cb
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-11-30 14:51:09 -0800

    fix tests

--- packages/bindings/test/oboe.test.ts
+++ packages/bindings/test/oboe.test.ts
@@ -14,60 +14,56 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import * as fs from "node:fs/promises"
-
-import { before, describe, expect, it } from "@solarwinds-apm/test"
+import { describe, expect, it } from "@solarwinds-apm/test"
 
 import { oboe } from ".."
 
 const INIT_TIMEOUT = 10_000
 
 describe("Reporter", () => {
-  before(async () => {
-    if (process.env.TEST_TRUSTEDPATH) {
-      const certificates = await fs.readFile(process.env.TEST_TRUSTEDPATH, {
-        encoding: "utf8",
-      })
-      process.env.TEST_CERTIFICATES = certificates
-    }
+  it("should initialise with service key and collector", () => {
+    const reporter = new oboe.Reporter({
+      service_key: process.env.TEST_SERVICE_KEY!,
+      host: process.env.TEST_COLLECTOR ?? "",
+      certificates: process.env.TEST_CERTIFICATES ?? "",
+
+      hostname_alias: "",
+      log_level: -2,
+      log_file_path: "",
+
+      max_transactions: -1,
+      max_flush_wait_time: -1,
+      events_flush_interval: -1,
+      max_request_size_bytes: -1,
+
+      reporter: "",
+
+      buffer_size: -1,
+      trace_metrics: 1,
+      histogram_precision: -1,
+      token_bucket_capacity: -1,
+      token_bucket_rate: -1,
+      file_single: 0,
+
+      ec2_metadata_timeout: -1,
+      grpc_proxy: "",
+      stdout_clear_nonblocking: 0,
+      metric_format: 0,
+
+      log_type: oboe.INIT_LOG_TYPE_NULL,
+    })
+    expect(reporter.init_status).to.equal(oboe.INIT_OK)
+
+    const ready = oboe.Context.isReady(INIT_TIMEOUT)
+    expect(ready).to.equal(oboe.SERVER_RESPONSE_OK)
   })
+})
 
-  it(
-    "should initialise with service key and collector",
-    () => {
-      const reporter = new oboe.Reporter({
-        service_key: process.env.TEST_SERVICE_KEY!,
-        host: process.env.TEST_COLLECTOR ?? "",
-        certificates: process.env.TEST_CERTIFICATES ?? "",
-
-        hostname_alias: "",
-        log_level: -2,
-        log_file_path: "",
-
-        max_transactions: -1,
-        max_flush_wait_time: -1,
-        events_flush_interval: -1,
-        max_request_size_bytes: -1,
-
-        reporter: "",
-
-        buffer_size: -1,
-        trace_metrics: 1,
-        histogram_precision: -1,
-        token_bucket_capacity: -1,
-        token_bucket_rate: -1,
-        file_single: 0,
-
-        ec2_metadata_timeout: -1,
-        grpc_proxy: "",
-        stdout_clear_nonblocking: 0,
-        metric_format: 0,
-      })
-      expect(reporter.init_status).to.equal(oboe.INIT_OK)
-
-      const ready = oboe.Context.isReady(INIT_TIMEOUT)
-      expect(ready).to.equal(oboe.SERVER_RESPONSE_OK)
-    },
-    INIT_TIMEOUT,
-  )
+describe("Metadata", () => {
+  describe("makeRandom", () => {
+    it("should produce valid metadata", () => {
+      const random = oboe.Metadata.makeRandom()
+      expect(random.isValid()).to.be.true
+    })
+  })
 })
