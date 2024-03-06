213049b51e64134fa61e327f2f85ff5b5cdb571e
Raphaël Thériault <raphael.theriault@solarwinds.com>
2024-03-05 19:16:50 -0800

    remove buggy reporter test

--- packages/bindings/test/oboe.test.ts
+++ packages/bindings/test/oboe.test.ts
@@ -18,47 +18,6 @@ import { describe, expect, it } from "@solarwinds-apm/test"
 
 import { oboe } from ".."
 
-const INIT_TIMEOUT = 10_000
-
-describe("Reporter", () => {
-  it("should initialise with service key and collector", () => {
-    const reporter = new oboe.Reporter({
-      service_key: process.env.TEST_SERVICE_KEY!,
-      host: process.env.TEST_COLLECTOR ?? "",
-      certificates: process.env.TEST_CERTIFICATES ?? "",
-
-      hostname_alias: "",
-      log_level: -2,
-      log_file_path: "",
-
-      max_transactions: -1,
-      max_flush_wait_time: -1,
-      events_flush_interval: -1,
-      max_request_size_bytes: -1,
-
-      reporter: "",
-
-      buffer_size: -1,
-      trace_metrics: 1,
-      histogram_precision: -1,
-      token_bucket_capacity: -1,
-      token_bucket_rate: -1,
-      file_single: 0,
-
-      ec2_metadata_timeout: -1,
-      grpc_proxy: "",
-      stdout_clear_nonblocking: 0,
-      metric_format: 0,
-
-      log_type: oboe.INIT_LOG_TYPE_NULL,
-    })
-    expect(reporter.init_status).to.equal(oboe.INIT_OK)
-
-    const ready = oboe.Context.isReady(INIT_TIMEOUT)
-    expect(ready).not.to.be.greaterThan(oboe.SERVER_RESPONSE_OK)
-  }).timeout(INIT_TIMEOUT)
-})
-
 describe("Metadata", () => {
   describe("makeRandom", () => {
     it("should produce valid metadata", () => {
