573c7cebe615b5ede6d892651329b33a4a4aaa8c
Raphaël Thériault <raphael.theriault@solarwinds.com>
2025-01-02 14:51:48 -0500

    it's that time of the year again

--- packages/solarwinds-apm/src/commonjs/api.js
+++ packages/solarwinds-apm/src/commonjs/api.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
-Copyright 2023-2024 SolarWinds Worldwide, LLC.
+Copyright 2023-2025 SolarWinds Worldwide, LLC.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
