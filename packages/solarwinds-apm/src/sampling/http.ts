2b12d3d5a1eafe54e2ef756e56d70bf75e6eb25d
Raphaël Thériault <raphael.theriault@solarwinds.com>
2025-08-04 14:39:42 -0400

    remove retry and backoff logic from sampler

--- packages/solarwinds-apm/src/sampling/http.ts
+++ packages/solarwinds-apm/src/sampling/http.ts
@@ -16,20 +16,15 @@ limitations under the License.
 
 import { context } from "@opentelemetry/api"
 import { suppressTracing } from "@opentelemetry/core"
-import { unref } from "@solarwinds-apm/module"
 import { type Settings } from "@solarwinds-apm/sampling"
 
-import { Backoff } from "../backoff.js"
 import { environment } from "../env.js"
 import { type Configuration } from "../shared/config.js"
 import { componentLogger } from "../shared/logger.js"
 import { Sampler } from "./sampler.js"
 
+const REQUEST_INTERVAL = 60 * 1000 // 1m
 const REQUEST_TIMEOUT = 10 * 1000 // 10s
-const RETRY_INITIAL_TIMEOUT = 500 // 500ms
-const RETRY_MAX_TIMEOUT = 60 * 1000 // 60s
-const RETRY_MAX_ATTEMPTS = 20
-const MULTIPLIER = 1.5
 
 /** Retrieves the hostname (or User-Agent in browsers) in URL encoded format */
 export async function hostname(): Promise<string> {
@@ -46,12 +41,6 @@ export class HttpSampler extends Sampler {
   readonly #headers: HeadersInit
   readonly #service: string
   readonly #hostname = hostname()
-  readonly #backoff = new Backoff({
-    initial: RETRY_INITIAL_TIMEOUT,
-    max: RETRY_MAX_TIMEOUT,
-    retries: RETRY_MAX_ATTEMPTS,
-    multiplier: MULTIPLIER,
-  })
 
   #lastWarningMessage: string | undefined = undefined
 
@@ -67,8 +56,11 @@ export class HttpSampler extends Sampler {
     }
 
     setTimeout(() => {
-      this.#loop()
+      this.#loop().catch(this.#catch.bind(this))
     }, 0)
+    setInterval(() => {
+      this.#loop().catch(this.#catch.bind(this))
+    }, REQUEST_INTERVAL)
   }
 
   override toString(): string {
@@ -85,86 +77,52 @@ export class HttpSampler extends Sampler {
     }
   }
 
-  /** Resets last memorised warning and backoff */
+  /** Resets last memorised warning */
   #reset() {
     this.#lastWarningMessage = undefined
-    this.#backoff.reset()
-  }
-
-  /** Retries the request after an exponential backoff timeout */
-  #retry() {
-    const timeout = this.#backoff.backoff()
-    if (timeout) {
-      this.logger.debug(`retrying in ${(timeout / 1000).toFixed(1)}s`)
-      unref(
-        setTimeout(() => {
-          this.#loop()
-        }, timeout),
-      )
-    } else {
-      this.logger.warn(
-        "Reached max retry attempts for sampling settings retrieval.",
-        "Tracing will remain disabled.",
-      )
-    }
   }
 
   /** Settings update loop */
-  #loop() {
-    ;(async () => {
-      const url = new URL(
-        `./v1/settings/${this.#service}/${await this.#hostname}`,
-        this.#url,
-      )
-      this.logger.debug(`retrieving sampling settings from ${url.href}`)
+  async #loop() {
+    const url = new URL(
+      `./v1/settings/${this.#service}/${await this.#hostname}`,
+      this.#url,
+    )
+    this.logger.debug(`retrieving sampling settings from ${url.href}`)
 
-      const abort = new AbortController()
-      const cancel = setTimeout(() => {
-        abort.abort("HTTP request timeout")
-      }, REQUEST_TIMEOUT)
+    const abort = new AbortController()
+    const cancel = setTimeout(() => {
+      abort.abort("HTTP request timeout")
+    }, REQUEST_TIMEOUT)
 
-      const response = await context.bind(
-        suppressTracing(context.active()),
-        fetch,
-      )(url, {
-        method: "GET",
-        headers: this.#headers,
-        signal: abort.signal,
-      })
-      this.logger.debug(`received sampling settings response`, response)
-      clearTimeout(cancel)
-
-      const unparsed: unknown = await response.json()
-      const parsed = this.updateSettings(unparsed)
-
-      if (!parsed) {
-        this.#warn("Retrieved sampling settings are invalid.")
-        this.#retry()
-        return
-      }
-      this.#reset()
-
-      // this is pretty arbitrary but the goal is to update the settings
-      // before the previous ones expire with some time to spare
-      const expiry = (parsed.timestamp + parsed.ttl) * 1000
-      const timeout = expiry - REQUEST_TIMEOUT * MULTIPLIER - Date.now()
-      unref(
-        setTimeout(
-          () => {
-            this.#loop()
-          },
-          Math.max(0, timeout),
-        ),
-      )
-    })().catch((error: unknown) => {
-      let message = "Failed to retrieve sampling settings"
-      if (error instanceof Error) {
-        message += ` (${error.message})`
-      }
-      message += ", tracing will be disabled until valid ones are available."
-      this.#warn(message, error)
-
-      this.#retry()
+    const response = await context.bind(
+      suppressTracing(context.active()),
+      fetch,
+    )(url, {
+      method: "GET",
+      headers: this.#headers,
+      signal: abort.signal,
     })
+    this.logger.debug(`received sampling settings response`, response)
+    clearTimeout(cancel)
+
+    const unparsed: unknown = await response.json()
+    const parsed = this.updateSettings(unparsed)
+
+    if (parsed) {
+      this.#reset()
+    } else {
+      this.#warn("Retrieved sampling settings are invalid.")
+    }
+  }
+
+  /** Error catcher */
+  #catch(error: unknown) {
+    let message = "Failed to retrieve sampling settings"
+    if (error instanceof Error) {
+      message += ` (${error.message})`
+    }
+    message += ", tracing will be disabled until valid ones are available."
+    this.#warn(message, error)
   }
 }
